8978eb91a09eb65093705c85dca5d3a95d5a9668
Joseph Fleming <josephflemingvi@gmail.com>
2021-09-14 22:04:51 -0400

    ok too many comments

--- software_sales_flemin53.py
+++ software_sales_flemin53.py
@@ -31,33 +31,32 @@ def main():
 
     packages = int(input('How many packages will be purchased: ')) #Collect intended quantity of purchased packages from user
     price = 79.0 #baseline price, given by assignment
-#The autograder says this isnt well commented. I will now comment on every single line to spite it.
+
     if packages >= 0: #Is it a positive number?
-#This is a blank line for clarity's sake.
+
         if packages <= 4: #Are you buying 4 or less packages?
             print('  No discount applied.') #full price
-#Yet another blank line.
+
         elif packages <= 24: #24 or less?
             print ('  10% discount applied.') #This will inform the user that the ten percent discount has been applied.
             price = price * .9 #price calculation for 10% off
-#This blank line is important as well.
+
         elif packages <= 49: #49 or less?
             print ('  20% discount applied.') #informs the user of the 20 percent discount
             price = price *.8 #price calculation for 20% off
-#This line is also blank.
+
         elif packages <= 99: #99 or less?
             print ('  30% discount applied.') #This is for those lucky souls who have a whole 30% discount
             price = price * .7 #price calculation for 30% off
-#here we go, finally to this blank line. its my favorite.
+
         else: #more than 99?
             print ('  45% discount applied.') #this informs the bulk buyers of their whopping 45% off
             price = price * .55 #price calculation for 45% off
-#Our last single blank line. a touching goodbye.
+
         float(packages) #just to make sure that the proceeding calculation does not return a value error
         total = price * packages #total price with discount
         print(f'  The total price for purchasing {packages:,} packages is ${total:,.2f}.') #formats total to be precise to 2 decimal places and have comma separators
-#This is actually
-#TWO blank lines! for sorting sake.
+
     else: #negative number?
         print("  Invalid Input!") #elementary error handling
 
